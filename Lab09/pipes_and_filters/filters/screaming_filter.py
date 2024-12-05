2a814db7539cf4bdc77e7915f2cf491564fbf22a
Mohamad Nour Shahin <o.shaheen1882@gmail.com>
2024-12-05 12:05:20 +0300

    remove trash staff

--- Lab09/pipes_and_filters/filters/screaming_filter.py
+++
@@ -1,7 +0,0 @@
-def apply_screaming(message):
-    print(f"SCREAMING Service: Received message: {message}")
-
-    message['text'] = message['text'].upper()
-    print(f"SCREAMING Service: Converted message to uppercase: {message}")
-
-    return message
